e8163a7c919fc8b2de266b1f1e43789c14070891
parvesht <enggparveshtandon@gmail.com>
2016-11-04 15:17:06 +0530

    Added HapiJs vision plugin demo code.

--- index.js
+++ index.js
@@ -1,33 +1,65 @@
-const HAPI = require('hapi');
+    const HAPI = require('hapi');
+    const PATH = require('path');
 
-const SERVER = new HAPI.Server();
+    const SERVER = new HAPI.Server();
 
-SERVER.connection({port:3000});
+    SERVER.connection({
+        port: 3000
+    });
 
-SERVER.route({
-    method: 'GET',
-    path: '/',
-    handler: function(request, reply){
-        
-        reply('Hello from hapiJs :-)');
-    }
-});
+    SERVER.register(require('vision'), function(err) {
 
-SERVER.route({
-    method: 'GET',
-    path: '/users/{userName}',
-    handler: function(request, reply){
-        
-        reply(`Hello from ${encodeURIComponent(request.params.userName)} !`);
-    }
-});
+        if (err) {
 
-SERVER.start((error) => {
+            throw err;
+        }
 
-    if (error){
+        SERVER.route({
+            method: 'GET',
+            path: '/01_vpd',
+            handler: function(request, reply) {
 
-        throw error;
-    }
+                reply.view('01_vision_plugin_demo', {
+                    title: 'HapiJs vision plugin demo',
+                    dscription: 'Basic demonstartion of how we can start with HapiJs <VISION> plugin and pass parameter to HTML file using <handlebars> template engine.',
+                });
+            }
+        });
+
+        SERVER.views({
+            engines: {
+                html: require('handlebars')
+            },
+            relativeTo: __dirname,
+            path: 'templates'
+        });
+
+    });
+
+    SERVER.route({
+        method: 'GET',
+        path: '/',
+        handler: function(request, reply) {
+
+            reply('Hello from hapiJs :-)');
+        }
+    });
+
+    SERVER.route({
+        method: 'GET',
+        path: '/users/{userName}',
+        handler: function(request, reply) {
+
+            reply(`Hello from ${encodeURIComponent(request.params.userName)} !`);
+        }
+    });
+
+    SERVER.start((error) => {
+
+        if (error) {
+
+            throw error;
+        }
 
         console.log(`Server running at ${SERVER.info.uri}`);
-});
+    });
